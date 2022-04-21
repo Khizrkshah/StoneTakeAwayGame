e9fd1113b3e495a0eff04a31daf1a891cef98cf1
Mian Khizr Shah <khizrkshah@gmail.com>
2022-04-22 01:26:06 +0200

    Added boxes variable and function initializeBoxes to BoxesState class

--- src/main/java/model/BoxesState.java
+++ src/main/java/model/BoxesState.java
@@ -1,5 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Class representing the state of the boxes.
+ */
 public class BoxesState {
+
+    public ArrayList<Integer> boxes = new ArrayList<>(15);
+
+    /**
+     * Initializes the ArrayList of boxes.
+     */
+    public void initializeBoxes(){
+        Random random = new Random();
+        int emptyBoxIndex = random.nextInt(14);
+        for (int i = 0; i < 15; i++){
+            if (i == emptyBoxIndex){
+                boxes.add(0);
+            }else{
+                boxes.add(1);
+            }
+        }
+
+    }
 
 }
