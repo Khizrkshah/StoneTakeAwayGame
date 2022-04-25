a478e8d95dc20a40a1c725a931a0ebd657b93675
Mian Khizr Shah <khizrkshah@gmail.com>
2022-04-25 04:02:06 +0200

    Made Changes to MainMenu FXML file and generated variables and methods for MainMenuController using SceneBuilder

--- src/main/java/controllers/MainMenuController.java
+++ src/main/java/controllers/MainMenuController.java
@@ -1,4 +1,40 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class MainMenuController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button highScoresButton;
+
+    @FXML
+    private TextField playerOneNameInput;
+
+    @FXML
+    private TextField playerTwoNameInput;
+
+    @FXML
+    private Button startGameButton;
+
+    @FXML
+    void exitPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void highScoresPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startGamePressed(ActionEvent event) {
+
+    }
+
 }
