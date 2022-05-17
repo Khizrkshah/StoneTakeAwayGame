62808635323ff4c3d566d1f5c5483dbab7238e1c
Mian Khizr Shah <khizrkshah@gmail.com>
2022-05-17 06:34:29 +0200

    Added Junit testing for ResultState Class, new file ResultStateTest in src/test/java/ tested setNumberOfWins, setWinner, equals and testHashcode methods

---
+++ src/test/java/model/ResultStateTest.java
@@ -0,0 +1,40 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import javax.xml.transform.Result;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultStateTest {
+
+    @Test
+    void setNumberOfMoves() {
+        ResultState resultState = new ResultState();
+        assertEquals(resultState.getNumberOfMoves(),0);
+        resultState.setNumberOfMoves(1);
+        assertEquals(resultState.getNumberOfMoves(),1);
+    }
+
+    @Test
+    void setWinner() {
+        ResultState resultState = new ResultState();
+        assertEquals(resultState.getWinner(),null);
+        resultState.setWinner("winner");
+        assertEquals(resultState.getWinner(),"winner");
+    }
+
+    @Test
+    void equals(){
+        ResultState resultState = new ResultState();
+        assertTrue(resultState.equals(resultState));
+        assertFalse(resultState.equals(new ResultState()));
+    }
+
+    @Test
+    void testHashCode() {
+        ResultState resultState = new ResultState();
+        ResultState resultState1 = new ResultState();
+        assertNotEquals(resultState.hashCode(),resultState1.hashCode());
+    }
+}
