ac1cf7949d007c58e4a9843386e61038ea765b61
Mian Khizr Shah <khizrkshah@gmail.com>
2022-05-17 03:28:23 +0200

    Added junit test for move method in GameStateTest class in src/test/java/model

--- src/test/java/model/GameStateTest.java
+++ src/test/java/model/GameStateTest.java
@@ -2,8 +2,10 @@ package model;
 
 import controllers.GamePlayController;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +16,18 @@ class GameStateTest {
     PlayerState playerState = new PlayerState();
 
 
-
     @Test
     void move() {
+        GameState model = new GameState();
+        int nonHiddenBox = 0;
+        for(int i = 0; i < model.numberOfBoxes; i++){
+            if (model.getSquare(i).toString() == " 0 "){
+             nonHiddenBox = i;
+             break;
+            }
+        }
+        model.move(nonHiddenBox,model,playerState);
+        assertEquals(model.getSquare(nonHiddenBox),new ReadOnlyObjectWrapper<>(model.getSquare(nonHiddenBox)).getValue());
 
     }
 
